187cb5809471577def4b42013f9fa8b655dedff3
jstelte <jstelte@outlook.com>
2021-10-26 20:24:33 -0600

    grid created

--- sketch.js
+++ sketch.js
@@ -1,8 +1,29 @@
+let grid = [];
+let w  = 20;
+let cols, rows;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
+	createCanvas(400, 400);
+	//calculate the amount of columns by dividing the width of the canvas by the size of the cells
+	cols = floor(width / w);
+	rows = floor(height / w);
+
+	for(let i = 0; i < rows; i++){
+		for(let j = 0; j < cols; j++){
+			let cell = new Cell(j, i, w);
+			grid.push(cell);
+		}
+	}
+
+	noLoop();
+
 }
 
 function draw() {
-	// put drawing code here
+	background(220);
+
+	for(let i = 0; i < grid.length; i++){
+		grid[i].show();
+	}
+
 }
